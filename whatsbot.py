8347eb9fd4ac55d4d36720a57d0e8fca26eb1fc2
Kleber Silva <kleber.silva.kgns2@outlook.com>
2021-01-30 02:20:41 -0300

    finaly, i got to enter in whatsapp web

--- whatsbot.py
+++ whatsbot.py
@@ -1,1 +1,14 @@
-print('Hello world!')
+#Importar bibliotecas
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+PATH = "chromedriver.exe"
+#Navegar até o whatsapp web
+driver = webdriver.Chrome(PATH)
+driver.get("https://web.whatsapp.com/")
+driver.maximize_window()
+time.sleep(30)
+#Definir contato e mensagem para enviar
+contatos = ['Evandro', 'Bianca']
+#Buscar o contato
+#Enviar mensagem
